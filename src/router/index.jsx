8b8458763913becdcf9201a18826ede1c01533c5
Alek30k <alemank23k@gmail.com>
2023-01-26 15:40:32 -0300

    last commit

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import LayoutPublic from "../layout/LayoutPublic";
 import About from "../pages/About";
 import Blog from "../pages/Blog";
 import Home from "../pages/Home";
@@ -7,17 +8,23 @@ import NotFound from "../pages/NotFound";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/blog",
-    element: <Blog />,
-    errorElement: <NotFound />,
+    element: <LayoutPublic />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+        errorElement: <NotFound />,
+      },
+    ],
   },
 ]);
