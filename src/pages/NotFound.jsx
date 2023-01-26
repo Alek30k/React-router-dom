f07e915b4c1b9e8363e766aa16768f57c3758e50
Alek30k <alemank23k@gmail.com>
2023-01-26 15:34:57 -0300

    last commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,13 @@ const NotFound = () => {
   const error = useRouteError();
   console.log(error);
 
-  return <div>"Not Found"</div>;
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <p>{error.statusText || error.message}</p>
+    </div>
+  );
 };
 
 export default NotFound;
