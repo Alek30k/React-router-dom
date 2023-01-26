4928fcba1cac70a6c74a722ac7c9842dafe92334
Alek30k <alemank23k@gmail.com>
2023-01-26 15:28:41 -0300

    first commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import { useRouteError } from "react-router-dom";
+
+const NotFound = () => {
+  const error = useRouteError();
+  console.log(error);
+
+  return <div>"Not Found"</div>;
+};
+
+export default NotFound;
