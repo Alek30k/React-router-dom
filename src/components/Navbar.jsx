7de4b0604614b34b1ece544476808c9e462e9167
Alek30k <alemank23k@gmail.com>
2023-01-26 15:59:49 -0300

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <NavLink to="/">Home</NavLink>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
