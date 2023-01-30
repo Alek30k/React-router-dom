90fa4a23a38e8e91f3fbcfbacc946b41f9ab44e8
Alek30k <alemank23k@gmail.com>
2023-01-30 16:06:39 -0300

    last commit

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,3 +3,9 @@ const Post = () => {
 };
 
 export default Post;
+
+export const loaderPost = async ({ params }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+};
